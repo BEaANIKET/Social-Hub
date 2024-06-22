7e864b35978801d89a4ca7eda8c831b19c262c45
BEaANIKET <aniketchaturvedi309@gmail.com>
2024-06-22 14:34:02 +0530

    index.js

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
-import dotenv from 'dotenv'
-import cors from 'cors'
-import express from 'express';
-import { connectDb } from './Db/index.js';
-import { router } from './routes/auth.js';
-import { postRouter } from './routes/post.js';
-import cookieParser from 'cookie-parser';
-import { userRoute } from './routes/user.routes.js';
+import dotenv from "dotenv";
+import cors from "cors";
+import express from "express";
+import { connectDb } from "./Db/index.js";
+import { router } from "./routes/auth.js";
+import { postRouter } from "./routes/post.js";
+import cookieParser from "cookie-parser";
+import { userRoute } from "./routes/user.routes.js";
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(cors())
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-app.use(express.json())
-app.use(cookieParser())
-dotenv.config()
+app.use(express.json());
+app.use(cookieParser());
+dotenv.config();
 
 connectDb();
-app.get('/', (req, res) =>{
-   res.send("hello I am Working ")
-})
+app.get("/", (req, res) => {
+  res.send("hello I am Working ");
+});
 
-app.use('/api', router)
-app.use('/api', postRouter)
-app.use('/api', userRoute)
+app.use("/api", router);
+app.use("/api", postRouter);
+app.use("/api", userRoute);
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
